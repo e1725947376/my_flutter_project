1f477464720dca6638d58e940beb727a407dc05e
AndresHaro1991 <andres.hr.1991@gmail.com>
2022-06-22 17:02:03 -0500

    textformfields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,63 +1,95 @@
+import 'package:fl_voy_app/ui/input_decorations.dart';
 import 'package:flutter/material.dart';
 
 import 'package:fl_voy_app/widgets/widgets.dart';
 
-
 class LoginScreen extends StatelessWidget {
-   
   const LoginScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthBackground(
-        child: SingleChildScrollView(
-           child: Column(
-            children:  [
-              const SizedBox(height: 100),
-              CardContainer(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    Text('Login',style: Theme.of(context).textTheme.headline4),
-                    const SizedBox(height: 30),
-                    _LoginForm(),
-
-
-                ],
-                ),
-                
-              ),
-              const SizedBox(height:50),
-              const Text('Registrar nueva cuenta',style: TextStyle(fontSize: 18))
-            ],
-           ),
-        )
-      )
-      );
-     
-    
+        body: AuthBackground(
+            child: SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(height: 100),
+          CardContainer(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Text('Login', style: Theme.of(context).textTheme.headline4),
+                const SizedBox(height: 30),
+                _LoginForm(),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text('Registrar nueva cuenta', style: TextStyle(fontSize: 18))
+        ],
+      ),
+    )));
   }
 }
 
 class _LoginForm extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
     return Container(
-      //TODO: REFERENCIA KEY
       child: Form(
         child: Column(
-        
-        children: [
-          TextFormField()
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Ingrese correo',
+                  labelText: 'Correo',
+                  prefixIcon: Icons.alternate_email_sharp),
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = new RegExp(pattern);
 
-
-
-
-        ]),
-    ),
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'El valor ingresado no es un correo';
+              },
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Ingrese contraseña',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline),
+              validator: (value) {
+                return (value != null && value.length >= 6)
+                    ? null
+                    : 'La contraseña debe ser de 6 caracteres';
+              },
+            ),
+            const SizedBox(height: 5),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 80, vertical: 15),
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: () {})
+          ],
+        ),
+      ),
     );
   }
 }
