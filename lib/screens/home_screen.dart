c237286fc0b47c5b224591ced41169244adb0184
AndresHaro1991 <andres.hr.1991@gmail.com>
2022-06-26 09:28:45 -0500

    homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_voy_app/theme/app_theme.dart';
+import 'package:fl_voy_app/router/app_routes.dart';
+
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
-    );
+    final menuOptions = AppRoutes.menuOptions;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Menu'),
+        ),
+        //initialRoute: AppRoutes.initialRoute,
+        //routes: AppRoutes.getAppRoutes(),
+        //onGenerateRoute: AppRoutes.onGenerateRoute,
+        //theme: AppTheme.lightTheme);
+
+        body: ListView.separated(
+            itemBuilder: (context, i) => ListTile(
+                  leading: Icon(menuOptions[i].icon, color: AppTheme.primary),
+                  title: Text(menuOptions[i].name),
+                  onTap: () {
+                    // final route = MaterialPageRoute(
+                    //   builder: (context) => const Listview1Screen(),
+                    // );
+                    // Navigator.push(context, route );
+
+                    Navigator.pushNamed(context, menuOptions[i].route);
+                  },
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: menuOptions.length));
   }
 }
